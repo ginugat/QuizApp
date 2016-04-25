5fe51173222a4e2415a33b635073d3d0587117d8
ginugat <ginugat@gmail.com>
2016-04-25 09:16:32 +0530

    Resolved previous issue

--- quizApplication/Program.cs
+++ quizApplication/Program.cs
@@ -16,40 +16,32 @@ namespace quizzAp
             foreach (var str in substr)
                 Console.WriteLine(str);
         }
+        public static int score = 0;
+        public static int wrong = 0;
+        public static int correct = 0;
+
         public static void AnsEval(int j)
         {
-            int score = 0;
-            int wrong = 0;
-            int correct = 0;
             string[] ans = File.ReadAllLines(@"C:\Users\Public\TestFolder\ans.txt");
             string correctans = ans[j];
-
-
             string userans = Console.ReadLine();
             if (correctans != userans)
             {
                 Console.WriteLine("Oopss wrong ans!");
                 Console.WriteLine("The correct answer is {0}", correctans);
-
                 score -= 1;
-                wrong++;
+                wrong += 1;
             }
             else
             {
                 Console.WriteLine("Correct ans");
                 score += 3;
-                correct++;
+                correct += 1;
             }
-
             Console.WriteLine("Press enter to continue!");
             Console.ReadLine();
             Console.Clear();
-
-
-            Console.WriteLine(score);
-
         }
-
         static void Main()
         {
             string[] intro = System.IO.File.ReadAllLines(@"C:\Users\Public\TestFolder\WriteText.txt");
@@ -60,20 +52,21 @@ namespace quizzAp
             }
             Console.WriteLine("Press any key to start the quiz");
             Console.ReadLine();
-
             Console.Clear();
 
-            for (int k = 0; k < 4; k++)
+            for (int k = 0; k < 3; k++)
             {
                 ReadText(k);
                 AnsEval(k);
             }
-            Console.WriteLine("Your score is {0}");
-
+            Console.WriteLine("Your score is {0}", ReadFromFile.score);
+            Console.WriteLine("Press enter to view the summary");
             Console.ReadLine();
-
-
-
+            Console.WriteLine("****Summaryy****");
+            Console.WriteLine("Number of questions answered correctly {0}", ReadFromFile.correct);
+            Console.WriteLine("Number of questions NOT answered correctly {0}", ReadFromFile.wrong);
+            Console.ReadLine();
         }
     }
 }
+
